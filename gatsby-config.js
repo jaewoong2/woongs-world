fa144878b47b9646a6ce7072bf4e9444c7c248d2
jaewoong2 <jwisgenius@naver.com>
2021-07-21 18:06:59 +0900

    환경설정 완료

---
+++ gatsby-config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  siteMetadata: {
+    title: '@JaeWoong2',
+    author: 'JaeWoong Lim',
+    description: 'prepare For FE developer',
+    siteUrl: 'https://jaewoong2.github.io/',
+  },
+  pathPrefix: '/woongs-world',
+  plugins: [
+    {
+      resolve: 'gatsby-plugin-typescript',
+      options: {
+        isTSX: true,
+        allExtensions: true,
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-image`,
+    // {
+    //   resolve: `gatsby-source-filesystem`,
+    //   options: {
+    //     name: `images`,
+    //     path: `${__dirname}/contents`,
+    //   },
+    // },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+};
